0ffaccbfa7fd33388cf06ddaa02351e333b599ae
Martin Murrer <martin@seventy2eleven.de>
2021-07-02 06:48:45 +0200

    Updates context debug.

--- game/Debug/ContextDebug.gd
+++ game/Debug/ContextDebug.gd
@@ -1,17 +1,33 @@
 extends CanvasLayer
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var labels = {}
 
+onready var label_container = $Control/HBoxContainer/VBoxContainer
 onready var cities_label = $Control/HBoxContainer/VBoxContainer/CitiesLabel
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	add_info("turn", "Turn: %d", 0)
+	add_info("turn1", "Turn: %s", "n/a")
+	add_info("turn2", "Turn: %s", null)
 
 
 func _process(delta):
-	var context = GameContext.get_statistics()
-	cities_label.text = "Cities: %d" % context.cities.count
+	update_info("turn",0)
+
+
+func update_info(key, value):
+	labels[key].label.text = labels[key].label_text % value
+
+
+func add_info(key, label_text, default_value):
+	var label = Label.new();
+	label.text = label_text % default_value;
+	label_container.add_child(label);
+	labels[key] = {
+		"default_value" : default_value,
+		"label_text" : label_text,
+		"label" : label
+		}
+	
